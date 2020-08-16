6f5f66030d630cc0d175544eebfd1ae48e15ef9d
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-16 01:28:41 +0100

    edit baseurl

--- front-end/src/js/utils/urls.js
+++ front-end/src/js/utils/urls.js
@@ -1,10 +1,8 @@
 export const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "http://ec2-3-248-208-68.eu-west-1.compute.amazonaws.com"
-    : "http://localhost:5000";
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:5000/";
 
-export const TASKS_EP = `/v1/api/tasks`;
-export const AUTH_EP = `/v1/api/auth`;
+export const TASKS_EP = `v1/api/tasks`;
+export const AUTH_EP = `v1/api/auth`;
 
 export const PARAMS = {
   baseURL,
