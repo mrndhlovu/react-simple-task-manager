41fbebcdf74e4e22b47d239d53a46787ed13e524
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-16 12:23:02 +0100

    edit baseurl

--- front-end/src/js/utils/urls.js
+++ front-end/src/js/utils/urls.js
@@ -1,9 +1,10 @@
-export const baseURL = "/";
+export const baseURL = "http://localhost:5000/";
 
-export const TASKS_EP = `/v1/api/tasks`;
-export const AUTH_EP = `/v1/api/auth`;
+export const TASKS_EP = `v1/api/tasks`;
+export const AUTH_EP = `v1/api/auth`;
 
 export const PARAMS = {
+  baseURL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -11,19 +12,3 @@ export const PARAMS = {
   credentials: "same-origin",
   withCredentials: true,
 };
-
-export const parseUrl = (search) => {
-  const searchParams = search.replace(/^\?/, "").split("&");
-  let result = {};
-  searchParams.map((query) => {
-    const data = query.split("=");
-    return (result = {
-      ...result,
-      [`${data.shift()}`]: data.shift(),
-    });
-  });
-
-  return result;
-};
-
-export const getQueryString = (location) => location.search.slice(1);
