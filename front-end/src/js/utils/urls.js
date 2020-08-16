2874b24b712b1df37f40bda820f5978308367488
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-16 12:43:53 +0100

    edit baseurl

--- front-end/src/js/utils/urls.js
+++ front-end/src/js/utils/urls.js
@@ -1,4 +1,7 @@
-export const baseURL = "http://localhost:5000/";
+export const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "http://ec2-3-248-208-68.eu-west-1.compute.amazonaws.com"
+    : "http://localhost:5000/";
 
 export const TASKS_EP = `v1/api/tasks`;
 export const AUTH_EP = `v1/api/auth`;
@@ -12,3 +15,19 @@ export const PARAMS = {
   credentials: "same-origin",
   withCredentials: true,
 };
+
+export const parseUrl = (search) => {
+  const searchParams = search.replace(/^\?/, "").split("&");
+  let result = {};
+  searchParams.map((query) => {
+    const data = query.split("=");
+    return (result = {
+      ...result,
+      [`${data.shift()}`]: data.shift(),
+    });
+  });
+
+  return result;
+};
+
+export const getQueryString = (location) => location.search.slice(1);
