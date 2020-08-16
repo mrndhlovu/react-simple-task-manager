33780c91947138929b0e57fa972ba70e26c47ed6
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-16 02:01:27 +0100

    edit baseurl

--- back-end/utils/config.js
+++ back-end/utils/config.js
@@ -5,7 +5,6 @@ dotenv.config();
 const isDevelopment = process.env.DEVELOPMENT === "development";
 
 const S_GRID_API_KEY = process.env.SEND_GRID_API_KEY;
-
 const CONNECTION_URI = process.env.MONGODB_URI;
 const { LOCAL_MONGO_DB } = process.env;
 
@@ -14,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const { PRIVATE_SIGNATURE, PUBLIC_SIGNATURE } = process.env;
 const BASE_URL = isDevelopment
   ? "http://localhost:3000"
-  : `http://ec2-3-248-208-68.eu-west-1.compute.amazonaws.com:${PORT}`;
+  : `http://ec2-3-248-208-68.eu-west-1.compute.amazonaws.com:5000`;
 
 const ALLOWED_UPDATE_FIELDS_USER = [
   "firstName",
