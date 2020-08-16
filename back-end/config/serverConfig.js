a4f359ef4f52692cbc322e95c8616c8b44a2dbec
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-16 01:43:47 +0100

    edit cors config

--- back-end/config/serverConfig.js
+++ back-end/config/serverConfig.js
@@ -7,7 +7,12 @@ const BUILD_DIR = "../../front-end/build";
 const serverConfig = (app, express) => {
   app.use(express.json());
   app.use(cookieParser());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: BASE_URL,
+      credentials: true,
+    })
+  );
 
   app.use(express.static(BUILD_DIR));
 };
