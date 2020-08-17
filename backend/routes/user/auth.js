522998aea2b8aaf1e14b50135847db9487a47273
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-17 20:35:16 +0100

    remove req timeout

--- backend/routes/user/auth.js
+++ backend/routes/user/auth.js
@@ -36,7 +36,6 @@ router.post("/register", async (req, res, next) => {
 });
 
 router.post("/login", async (req, res, next) => {
-  req.connection.setTimeout(100000);
   const { email, password } = req.body;
 
   try {
