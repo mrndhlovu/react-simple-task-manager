fe9d6da95557840eb1684f5aa7932bf58fb995e9
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-17 17:00:34 +0100

    edit serverconfig

--- backend/config/routesConfig.js
+++ backend/config/routesConfig.js
@@ -2,9 +2,11 @@ const authRoutes = require("../routes/user/auth");
 const taskRoutes = require("../routes/user/tasks");
 const errorHandler = require("./errorHandler");
 
+const API_SUFFIX = "/v1/api";
+
 const routesConfig = (app) => {
-  app.use("/v1/api/auth", authRoutes);
-  app.use("/v1/api/tasks", taskRoutes);
+  app.use(`${API_SUFFIX}/auth`, authRoutes);
+  app.use(`${API_SUFFIX}/tasks`, taskRoutes);
 
   app.use(errorHandler);
 };
