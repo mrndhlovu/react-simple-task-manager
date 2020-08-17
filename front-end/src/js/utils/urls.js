daa631b3d0b1e64544cf5bf04dda33553e9e6212
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-17 09:11:14 +0100

    update base url

--- front-end/src/js/utils/urls.js
+++ front-end/src/js/utils/urls.js
@@ -1,7 +1,10 @@
+export const baseURL = "http://localhost:5000/";
+
 export const TASKS_EP = `v1/api/tasks`;
 export const AUTH_EP = `v1/api/auth`;
 
 export const PARAMS = {
+  baseURL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
