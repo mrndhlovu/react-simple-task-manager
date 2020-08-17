ec151d3ea11d84ff679f126befe1cf2665a6cefe
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-17 15:45:37 +0100

    edit baseurl

--- frontend/src/js/utils/urls.js
+++ frontend/src/js/utils/urls.js
@@ -1,7 +1,7 @@
 export const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:5000"
-    : "http://172.31.33.194";
+    : "http://34.245.58.144";
 
 export const TASKS_EP = "/v1/api/tasks";
 export const AUTH_EP = "/v1/api/auth";
