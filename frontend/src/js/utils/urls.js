35e02b4c08a075fee50d2167bf7623614c3b050f
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-18 15:34:04 +0100

    add subdomain url

--- frontend/src/js/utils/urls.js
+++ frontend/src/js/utils/urls.js
@@ -1,7 +1,7 @@
 export const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:5000"
-    : "http://ec2-34-245-58-144.eu-west-1.compute.amazonaws.com";
+    : "http://app.checklists.ndhlovu.com";
 
 export const TASKS_EP = "/api/tasks";
 export const AUTH_EP = "/api/auth";
