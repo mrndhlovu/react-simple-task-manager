0cac990ca059cc5e88e03a925bf7bd246b6243e4
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-18 22:06:08 +0100

    update baseurl

--- frontend/src/js/utils/urls.js
+++ frontend/src/js/utils/urls.js
@@ -1,7 +1,7 @@
 export const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:5000"
-    : "http://app.checklists.ndhlovu.com";
+    : "https://app.checklists.ndhlovu.com";
 
 export const TASKS_EP = "/api/tasks";
 export const AUTH_EP = "/api/auth";
